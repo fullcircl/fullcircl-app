7bee492ec3bad56c1468dfb882178230d27b00d9
Chad Kirby <x3haloed@gmail.com>
2021-04-09 08:29:34 -0700

    Moved nav bar into separate component

--- src/fullcircl-client/client-app/src/App.svelte
+++ src/fullcircl-client/client-app/src/App.svelte
@@ -1,45 +1,13 @@
 <script lang="ts">
-	import Tailwindcss from "./Tailwindcss.svelte";
+	import Navbar from "./Navbar.svelte";
+import Tailwindcss from "./Tailwindcss.svelte";
 
 	export let name: string;
 </script>
 
 <Tailwindcss />
 <main>
-	<nav class="bg-gray-800">
-		<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-			<div class="flex items-center justify-between h-16">
-				<div class="flex items-center">
-					<div class="flex-shrink-0">
-						<img class="h-8" src="logo-type.svg" alt="Fullcircl App" />
-					</div>
-				</div>
-				<div class="hidden md:block">
-					<div class="ml-4 flex items-center md:ml-6">
-						<!-- Profile dropdown -->
-						<div class="ml-3 relative">
-							<div>
-								<button
-									type="button"
-									class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-									id="user-menu"
-									aria-expanded="false"
-									aria-haspopup="true"
-								>
-									<span class="sr-only">Open user menu</span>
-									<img
-										class="h-8 w-8 rounded-full"
-										src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-										alt=""
-									/>
-								</button>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</nav>
+	<Navbar />
 </main>
 
 <style>
