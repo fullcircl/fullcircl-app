7d56d8845b21e7b26ec66d77031a2dabf386d153
Chad Kirby <x3haloed@gmail.com>
2021-04-09 08:52:34 -0700

    Added very basic input bar

--- src/fullcircl-client/client-app/src/App.svelte
+++ src/fullcircl-client/client-app/src/App.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
+	import InputBar from "./InputBar.svelte";
 	import Navbar from "./Navbar.svelte";
-import Tailwindcss from "./Tailwindcss.svelte";
-
-	export let name: string;
+	import Tailwindcss from "./Tailwindcss.svelte";
 </script>
 
 <Tailwindcss />
 <main>
 	<Navbar />
+	<InputBar />
 </main>
 
 <style>
