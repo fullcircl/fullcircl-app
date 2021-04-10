4414b3ecab99b00a4ddcc73aadb092687dbe0ed2
Chad Kirby <x3haloed@gmail.com>
2021-04-10 08:22:35 -0700

    Fixed input bar border

--- src/fullcircl-client/client-app/src/InputBar.svelte
+++ src/fullcircl-client/client-app/src/InputBar.svelte
@@ -1,5 +1,5 @@
     <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center">
       <div class="inline-block align-bottom transform transition-all">
-        <input type="text" name="message" id="message" placeholder="Message #General" class="mt-1 h-12 focus:ring-indigo-500 focus:border-indigo-500 block w-full border-gray-300 rounded-md">
+        <input type="text" name="message" id="message" placeholder="Message #General" class="mt-1 h-12 block w-full border border-gray-300 rounded-md">
       </div>
     </div>
