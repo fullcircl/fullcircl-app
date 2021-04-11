1397ae4ba41a2f106576e58218493a56f5ed77e9
Chad Kirby <x3haloed@gmail.com>
2021-04-11 09:11:11 -0700

    Added rudimentary autoheight

--- src/fullcircl-client/client-app/src/InputBar.svelte
+++ src/fullcircl-client/client-app/src/InputBar.svelte
@@ -1,7 +1,15 @@
+<script>
+  let message = '';
+  let taHeight = 0;
+</script>
+
 <div class="absolute px-4 bottom-4 w-full">
-  <div class="flex items-end h-12 justify-center text-center dark:bg-gray-300 rounded">
+  <div class="flex items-end dark:bg-gray-300 rounded">
     <div class="inline-block flex-1 align-bottom transform transition-all">
-      <textarea name="message" id="message" placeholder="Message #General" class="block w-full resize-none outline-none rounded-tl rounded-bl dark:text-white-50 dark:placeholder-white-900" />
+      <textarea bind:value={message} bind:clientHeight={taHeight} placeholder="Message #General" class="block w-full resize-none outline-none rounded-tl rounded-bl dark:text-white-50 dark:placeholder-white-900" />
+      <div bind:clientHeight={taHeight} class="block w-full dark:text-white-50 dark:placeholder-white-900">
+        { message }
+      </div>
     </div>
     <div class="inline-block flex-none">
       TT
