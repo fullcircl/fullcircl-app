04412d483eb8d121056879d2075b249fdd5d5fef
Chad Kirby <x3haloed@gmail.com>
2021-04-11 08:48:49 -0700

    Changed input to textarea

--- src/fullcircl-client/client-app/src/InputBar.svelte
+++ src/fullcircl-client/client-app/src/InputBar.svelte
@@ -1,7 +1,7 @@
 <div class="absolute px-4 bottom-4 w-full">
-  <div class="flex items-end h-12 justify-center text-center dark:bg-gray-300 rounded-md">
+  <div class="flex items-end h-12 justify-center text-center dark:bg-gray-300 rounded">
     <div class="inline-block flex-1 align-bottom transform transition-all">
-      <input type="text" name="message" id="message" placeholder="Message #General" class="block w-full dark:text-white-50 dark:placeholder-white-900">
+      <textarea name="message" id="message" placeholder="Message #General" class="block w-full resize-none outline-none rounded-tl rounded-bl dark:text-white-50 dark:placeholder-white-900" />
     </div>
     <div class="inline-block flex-none">
       TT
@@ -11,7 +11,7 @@
 
 <style>
   /* Force overwrite user agent stylesheet */
-  input {
-    background-color: #45403D !important;
+  textarea {
+    background-color: rgba(0, 0, 0, 0) !important;
   }
 </style>
