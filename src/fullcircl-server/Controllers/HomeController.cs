c72efab78005ffa6de8c310876fb4845a3b337d7
chad <x3haloed@gmail.com>
2021-04-12 16:48:33 -0700

    Moved redis connection logic into separate service

--- src/fullcircl-server/Controllers/HomeController.cs
+++ src/fullcircl-server/Controllers/HomeController.cs
@@ -1,12 +1,9 @@
-using System;
 using System.Collections.Generic;
 using System.Net.Mime;
-using System.Net.Sockets;
 using System.Text;
-using System.Threading;
+using Fullcircl.Server.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using StackExchange.Redis;
 
@@ -15,48 +12,14 @@ namespace Fullcircl.Server
     [Route("[controller]")]
     public class HomeController : ControllerBase
     {
-        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        public HomeController(ILogger<HomeController> logger, RedisConnectionService redis)
         {
             _logger = logger;
-            
-            if (Configuration == null)
-            {
-                lock(_syncRoot)
-                {
-                    if (Configuration == null)
-                    {
-                        Configuration = configuration;
-                    }
-                }
-            }
+            _redis = redis;
         }
 
-        private static readonly object _syncRoot = new {};
-
         private readonly ILogger<HomeController> _logger;
-        private static IConfiguration? Configuration { get; set; }
-
-        private const string SecretName = "RedisConnection";
-
-        private static long lastReconnectTicks = DateTimeOffset.MinValue.UtcTicks;
-        private static DateTimeOffset firstErrorTime = DateTimeOffset.MinValue;
-        private static DateTimeOffset previousErrorTime = DateTimeOffset.MinValue;
-
-        private static readonly object reconnectLock = new object();
-
-        // In general, let StackExchange.Redis handle most reconnects,
-        // so limit the frequency of how often ForceReconnect() will
-        // actually reconnect.
-        public static TimeSpan ReconnectMinFrequency => TimeSpan.FromSeconds(60);
-
-        // If errors continue for longer than the below threshold, then the
-        // multiplexer seems to not be reconnecting, so ForceReconnect() will
-        // re-create the multiplexer.
-        public static TimeSpan ReconnectErrorThreshold => TimeSpan.FromSeconds(30);
-
-        public static int RetryMaxAttempts => 5;
-
-        private static Lazy<ConnectionMultiplexer> lazyConnection = CreateConnection();
+        private readonly RedisConnectionService _redis;
 
         [HttpGet("redis-cache")]
         [Produces(MediaTypeNames.Application.Json)]
@@ -68,7 +31,7 @@ namespace Fullcircl.Server
                 { "Message", "A simple example with Azure Cache for Redis on ASP.NET Core." }
             };
 
-            IDatabase cache = GetDatabase();
+            IDatabase cache = _redis.GetDatabase();
 
             // Perform cache operations using the cache object...
 
@@ -91,8 +54,8 @@ namespace Fullcircl.Server
             // Note that this requires allowAdmin=true in the connection string
             result.Add("command5", "CLIENT LIST");
             StringBuilder sb = new StringBuilder();
-            var endpoint = (System.Net.DnsEndPoint)GetEndPoints()[0];
-            IServer server = GetServer(endpoint.Host, endpoint.Port);
+            var endpoint = (System.Net.DnsEndPoint)_redis.GetEndPoints()[0];
+            IServer server = _redis.GetServer(endpoint.Host, endpoint.Port);
             ClientInfo[] clients = server.ClientList();
 
             sb.AppendLine("Cache response :");
@@ -104,141 +67,6 @@ namespace Fullcircl.Server
             result.Add("command5Result", sb.ToString());
 
             return Ok(result);
-        }
-
-        public static ConnectionMultiplexer Connection
-        {
-            get
-            {
-                return lazyConnection.Value;
-            }
-        }
-
-        private static Lazy<ConnectionMultiplexer> CreateConnection()
-        {
-            return new Lazy<ConnectionMultiplexer>(() =>
-            {
-                string cacheConnection = Configuration[SecretName];
-                return ConnectionMultiplexer.Connect(cacheConnection);
-            });
-        }
-
-        private static void CloseConnection(Lazy<ConnectionMultiplexer> oldConnection)
-        {
-            if (oldConnection == null)
-                return;
-
-            try
-            {
-                oldConnection.Value.Close();
-            }
-            catch (Exception)
-            {
-                // Example error condition: if accessing oldConnection.Value causes a connection attempt and that fails.
-            }
-        }
-
-        /// <summary>
-        /// Force a new ConnectionMultiplexer to be created.
-        /// NOTES:
-        ///     1. Users of the ConnectionMultiplexer MUST handle ObjectDisposedExceptions, which can now happen as a result of calling ForceReconnect().
-        ///     2. Don't call ForceReconnect for Timeouts, just for RedisConnectionExceptions or SocketExceptions.
-        ///     3. Call this method every time you see a connection exception. The code will:
-        ///         a. wait to reconnect for at least the "ReconnectErrorThreshold" time of repeated errors before actually reconnecting
-        ///         b. not reconnect more frequently than configured in "ReconnectMinFrequency"
-        /// </summary>
-        public static void ForceReconnect()
-        {
-            var utcNow = DateTimeOffset.UtcNow;
-            long previousTicks = Interlocked.Read(ref lastReconnectTicks);
-            var previousReconnectTime = new DateTimeOffset(previousTicks, TimeSpan.Zero);
-            TimeSpan elapsedSinceLastReconnect = utcNow - previousReconnectTime;
-
-            // If multiple threads call ForceReconnect at the same time, we only want to honor one of them.
-            if (elapsedSinceLastReconnect < ReconnectMinFrequency)
-                return;
-
-            lock (reconnectLock)
-            {
-                utcNow = DateTimeOffset.UtcNow;
-                elapsedSinceLastReconnect = utcNow - previousReconnectTime;
-
-                if (firstErrorTime == DateTimeOffset.MinValue)
-                {
-                    // We haven't seen an error since last reconnect, so set initial values.
-                    firstErrorTime = utcNow;
-                    previousErrorTime = utcNow;
-                    return;
-                }
-
-                if (elapsedSinceLastReconnect < ReconnectMinFrequency)
-                    return; // Some other thread made it through the check and the lock, so nothing to do.
-
-                TimeSpan elapsedSinceFirstError = utcNow - firstErrorTime;
-                TimeSpan elapsedSinceMostRecentError = utcNow - previousErrorTime;
-
-                bool shouldReconnect =
-                    elapsedSinceFirstError >= ReconnectErrorThreshold // Make sure we gave the multiplexer enough time to reconnect on its own if it could.
-                    && elapsedSinceMostRecentError <= ReconnectErrorThreshold; // Make sure we aren't working on stale data (e.g. if there was a gap in errors, don't reconnect yet).
-
-                // Update the previousErrorTime timestamp to be now (e.g. this reconnect request).
-                previousErrorTime = utcNow;
-
-                if (!shouldReconnect)
-                    return;
-
-                firstErrorTime = DateTimeOffset.MinValue;
-                previousErrorTime = DateTimeOffset.MinValue;
-
-                Lazy<ConnectionMultiplexer> oldConnection = lazyConnection;
-                CloseConnection(oldConnection);
-                lazyConnection = CreateConnection();
-                Interlocked.Exchange(ref lastReconnectTicks, utcNow.UtcTicks);
-            }
-        }
-
-        // In real applications, consider using a framework such as
-        // Polly to make it easier to customize the retry approach.
-        private static T BasicRetry<T>(Func<T> func)
-        {
-            int reconnectRetry = 0;
-            int disposedRetry = 0;
-
-            while (true)
-            {
-                try
-                {
-                    return func();
-                }
-                catch (Exception ex) when (ex is RedisConnectionException || ex is SocketException)
-                {
-                    reconnectRetry++;
-                    if (reconnectRetry > RetryMaxAttempts)
-                        throw;
-                    ForceReconnect();
-                }
-                catch (ObjectDisposedException)
-                {
-                    disposedRetry++;
-                    if (disposedRetry > RetryMaxAttempts)
-                        throw;
-                }
-            }
-        }
-
-        public static IDatabase GetDatabase()
-        {
-            return BasicRetry(() => Connection.GetDatabase());
-        }
-
-        public static System.Net.EndPoint[] GetEndPoints()
-        {
-            return BasicRetry(() => Connection.GetEndPoints());
-        }
-
-        public static IServer GetServer(string host, int port)
-        {
-            return BasicRetry(() => Connection.GetServer(host, port));
         }
     }
 }
---
+++ src/fullcircl-server/Services/RedisConnectionService.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
+
+namespace Fullcircl.Server.Services
+{
+    public class RedisConnectionService
+    {
+        public RedisConnectionService(ILogger<RedisConnectionService> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            _lazyConnection = CreateConnection();
+        }
+
+        private readonly ILogger<RedisConnectionService> _logger;
+        private readonly IConfiguration _configuration;
+        private Lazy<ConnectionMultiplexer> _lazyConnection;
+
+        private const string SecretName = "RedisConnection";
+
+        private long _lastReconnectTicks = DateTimeOffset.MinValue.UtcTicks;
+        private DateTimeOffset _firstErrorTime = DateTimeOffset.MinValue;
+        private DateTimeOffset _previousErrorTime = DateTimeOffset.MinValue;
+
+        private readonly object _reconnectLock = new { };
+
+        // In general, let StackExchange.Redis handle most reconnects,
+        // so limit the frequency of how often ForceReconnect() will
+        // actually reconnect.
+        public static TimeSpan ReconnectMinFrequency => TimeSpan.FromSeconds(60);
+
+        // If errors continue for longer than the below threshold, then the
+        // multiplexer seems to not be reconnecting, so ForceReconnect() will
+        // re-create the multiplexer.
+        public static TimeSpan ReconnectErrorThreshold => TimeSpan.FromSeconds(30);
+
+        public static int RetryMaxAttempts => 5;
+
+        public ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return _lazyConnection.Value;
+            }
+        }
+
+        private Lazy<ConnectionMultiplexer> CreateConnection()
+        {
+            return new Lazy<ConnectionMultiplexer>(() =>
+            {
+                string cacheConnection = _configuration[SecretName];
+                return ConnectionMultiplexer.Connect(cacheConnection);
+            });
+        }
+
+        private static void CloseConnection(Lazy<ConnectionMultiplexer> oldConnection)
+        {
+            if (oldConnection == null)
+                return;
+
+            try
+            {
+                oldConnection.Value.Close();
+            }
+            catch (Exception)
+            {
+                // Example error condition: if accessing oldConnection.Value causes a connection attempt and that fails.
+            }
+        }
+
+        /// <summary>
+        /// Force a new ConnectionMultiplexer to be created.
+        /// NOTES:
+        ///     1. Users of the ConnectionMultiplexer MUST handle ObjectDisposedExceptions, which can now happen as a result of calling ForceReconnect().
+        ///     2. Don't call ForceReconnect for Timeouts, just for RedisConnectionExceptions or SocketExceptions.
+        ///     3. Call this method every time you see a connection exception. The code will:
+        ///         a. wait to reconnect for at least the "ReconnectErrorThreshold" time of repeated errors before actually reconnecting
+        ///         b. not reconnect more frequently than configured in "ReconnectMinFrequency"
+        /// </summary>
+        public void ForceReconnect()
+        {
+            var utcNow = DateTimeOffset.UtcNow;
+            long previousTicks = Interlocked.Read(ref _lastReconnectTicks);
+            var previousReconnectTime = new DateTimeOffset(previousTicks, TimeSpan.Zero);
+            TimeSpan elapsedSinceLastReconnect = utcNow - previousReconnectTime;
+
+            // If multiple threads call ForceReconnect at the same time, we only want to honor one of them.
+            if (elapsedSinceLastReconnect < ReconnectMinFrequency)
+                return;
+
+            lock (_reconnectLock)
+            {
+                utcNow = DateTimeOffset.UtcNow;
+                elapsedSinceLastReconnect = utcNow - previousReconnectTime;
+
+                if (_firstErrorTime == DateTimeOffset.MinValue)
+                {
+                    // We haven't seen an error since last reconnect, so set initial values.
+                    _firstErrorTime = utcNow;
+                    _previousErrorTime = utcNow;
+                    return;
+                }
+
+                if (elapsedSinceLastReconnect < ReconnectMinFrequency)
+                    return; // Some other thread made it through the check and the lock, so nothing to do.
+
+                TimeSpan elapsedSinceFirstError = utcNow - _firstErrorTime;
+                TimeSpan elapsedSinceMostRecentError = utcNow - _previousErrorTime;
+
+                bool shouldReconnect =
+                    elapsedSinceFirstError >= ReconnectErrorThreshold // Make sure we gave the multiplexer enough time to reconnect on its own if it could.
+                    && elapsedSinceMostRecentError <= ReconnectErrorThreshold; // Make sure we aren't working on stale data (e.g. if there was a gap in errors, don't reconnect yet).
+
+                // Update the previousErrorTime timestamp to be now (e.g. this reconnect request).
+                _previousErrorTime = utcNow;
+
+                if (!shouldReconnect)
+                    return;
+
+                _firstErrorTime = DateTimeOffset.MinValue;
+                _previousErrorTime = DateTimeOffset.MinValue;
+
+                Lazy<ConnectionMultiplexer> oldConnection = _lazyConnection;
+                CloseConnection(oldConnection);
+                _lazyConnection = CreateConnection();
+                Interlocked.Exchange(ref _lastReconnectTicks, utcNow.UtcTicks);
+            }
+        }
+
+        // In real applications, consider using a framework such as
+        // Polly to make it easier to customize the retry approach.
+        private T BasicRetry<T>(Func<T> func)
+        {
+            int reconnectRetry = 0;
+            int disposedRetry = 0;
+
+            while (true)
+            {
+                try
+                {
+                    return func();
+                }
+                catch (Exception ex) when (ex is RedisConnectionException || ex is SocketException)
+                {
+                    reconnectRetry++;
+                    if (reconnectRetry > RetryMaxAttempts)
+                        throw;
+                    ForceReconnect();
+                }
+                catch (ObjectDisposedException)
+                {
+                    disposedRetry++;
+                    if (disposedRetry > RetryMaxAttempts)
+                        throw;
+                }
+            }
+        }
+
+        public IDatabase GetDatabase()
+        {
+            return BasicRetry(() => Connection.GetDatabase());
+        }
+
+        public System.Net.EndPoint[] GetEndPoints()
+        {
+            return BasicRetry(() => Connection.GetEndPoints());
+        }
+
+        public IServer GetServer(string host, int port)
+        {
+            return BasicRetry(() => Connection.GetServer(host, port));
+        }
+    }
+}
