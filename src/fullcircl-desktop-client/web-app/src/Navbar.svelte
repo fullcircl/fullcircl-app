75be921211cf82134dd5a9faa48a6d81701ebd5e
chad <x3haloed@gmail.com>
2021-04-15 09:36:47 -0700

    Swapped avatar picture

--- src/fullcircl-desktop-client/web-app/src/Navbar.svelte
+++ src/fullcircl-desktop-client/web-app/src/Navbar.svelte
@@ -21,7 +21,7 @@
                                 <span class="sr-only">Open user menu</span>
                                 <img
                                     class="h-7 w-7 rounded-full"
-                                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                    src="https://sguru.org/wp-content/uploads/2017/06/steam-avatar-profile-picture-0242.jpg"
                                     alt=""
                                 />
                             </button>
